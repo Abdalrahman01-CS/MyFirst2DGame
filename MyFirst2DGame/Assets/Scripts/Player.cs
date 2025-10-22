a3e2654c6a33dd6a20218c33224306c6315da1e6
Abdalrahman Mohamed <abdalrahman2018home@gmail.com>
2025-10-22 05:03:26 -0700

    second lab

--- MyFirst2DGame/Assets/Scripts/Player.cs
+++ MyFirst2DGame/Assets/Scripts/Player.cs
@@ -19,10 +19,12 @@ public class Player : MonoBehaviour
 
     private bool grounded;
 
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -56,6 +58,10 @@ public class Player : MonoBehaviour
                 GetComponent<SpriteRenderer>().flipX = false;
             }
         }
+
+        anim.SetFloat("Speed",Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+        anim.SetFloat("Height", GetComponent<Rigidbody2D>().velocity.y);
+        anim.SetBool("Grounded", grounded);
     }
 
     void Jump()
